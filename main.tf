f3735952990a373abbc58c25cd480e67e907774b
L00188525 <l00188525@atu.ie>
2024-11-23 16:50:44 +0000

    Feat: Add public and private subnets to stack

--- main.tf
+++ main.tf
@@ -33,6 +33,26 @@ resource "vpc" "my_vpc" {
   }
 }
 
+# Adding a public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "VPC public subnet"
+  }
+}
+
+# Adding a private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "VPC private subnet"
+  }
+}
+
 # Specifying instance parameters
 resource "aws_instance" "my_instance" {
   ami           = "ami-054a53dca63de757b"
